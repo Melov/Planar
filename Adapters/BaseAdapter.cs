d180880e1f9ce8989a4f95bbb9eb6443938cde28
Sergey Melov <tulonov.se@gmail.com>
2017-09-29 14:05:17 +0800

    Настройка скелета проекта

---
+++ Adapters/BaseAdapter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planar.Adapters
+{
+    public abstract class BaseAdapter
+    {
+        public abstract string Exec(string recipe);
+    }
+}
